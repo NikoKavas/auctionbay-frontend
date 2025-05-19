7afa066dd35a11be0da0fb64ddadcc126dd20df6
NikoKavas <niko.kavas@student.um.si>
2025-05-19 21:30:03 +0200

    Popravljena registracija da deluje

--- src/components/Form/InputField.tsx
+++ src/components/Form/InputField.tsx
@@ -1,21 +1,16 @@
-// src/components/Form/InputField.tsx
 import React, { type InputHTMLAttributes, useState } from 'react'
-import styled, { css } from 'styled-components'
-import { theme } from '../../styles/theme'
 import { FieldWrapper, StyledInput, StyledLabel, TogglePasswordIcon } from './InputField.styles'
 import eyeIcon from '../../assets/eye.png'
-
 
 interface InputFieldProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string
 }
 
-export const InputField: React.FC<InputFieldProps> = ({ label, type, ...props }) => {
+export const InputField: React.FC<InputFieldProps> = ({ label, type, value, onChange, ...props }) => {
   const [focused, setFocused] = useState(false)
-  const [value, setValue] = useState('')
   const [showPassword, setShowPassword] = useState(false)
 
-  const filled = value.trim().length > 0
+  const filled = Boolean(value && value.toString().length > 0)
   const isPassword = type === 'password'
 
   return (
@@ -26,11 +21,10 @@ export const InputField: React.FC<InputFieldProps> = ({ label, type, ...props })
           {...props}
           type={isPassword && !showPassword ? 'password' : 'text'}
           value={value}
-          onChange={(e) => setValue(e.target.value)}
+          onChange={onChange}
           onFocus={() => setFocused(true)}
           onBlur={() => setFocused(false)}
         />
-
         {isPassword && (
           <TogglePasswordIcon
             src={eyeIcon}
