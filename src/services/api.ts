656d8734ce46e4fe4e15c6d32909e0f1f412f076
NikoKavas <niko.kavas@student.um.si>
2025-10-23 23:01:01 +0200

    fix again

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,9 @@ import axios from 'axios'
 
 // Axios instanca, ki jo lahko kjerkoli uvoziš
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.DEV
+    ? 'http://localhost:3000'
+    : '/api',
   withCredentials: true, // httpOnly cookie za JWT
   headers: {
     'Content-Type': 'application/json'
